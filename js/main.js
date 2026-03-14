6763d2b4b98050ca65e1de832d5a264d98e7b018
MartineitoR <martintaraffo@gmail.com>
2026-03-13 23:21:13 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash');
+
+    setTimeout(() => {
+        splash.classList.add('subir');
+
+        setTimeout(() => {
+            splash.style.display = 'none';
+        }, 1000); 
+    }, 700); 
+});
+
+const menuBtn = document.getElementById('menuBtn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('activo');
+});
+
+const linksMenu = document.querySelectorAll("#menu a");
+
+linksMenu.forEach(function(link){
+    link.addEventListener("click", function(){
+        menu.classList.remove("activo");
+        });
+    });
+
+/*const botonMenu = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const linksMenu = document.querySelectorAll("#menu a");
+
+linksMenu.forEach(function(link){
+    link.addEventListener("click", function(){
+        menu.classList.remove("activo");
+        });
+    });
+
+botonMenu.addEventListener("click", function(){
+    menu.classList.toggle("activo");
+})*/
